ade7b8c0f7991b0cf8cc0018ac67f4c79e51a34c
Mykhailo Yaremchuk <120925743+mykhai1o@users.noreply.github.com>
2024-11-22 21:58:31 +0200

    Delete lesson_16 directory

--- lesson_16/js/script.js
+++
@@ -1,8 +0,0 @@
-const menuIcon = document.querySelector('.menu-header__button');
-
-
-document.addEventListener('click', (e) => {
-    if (e.target.closest('.menu-button')) {
-        document.documentElement.classList.toggle('open-menu');
-    }
-});
